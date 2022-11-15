93e842400910da9327cd49a08b671bf3f27e7df8
swarnim <swarnim.1212@gmail.com>
2022-11-15 12:44:28 +0530

    updated offensive security repo

--- cve_searchsploit/main.py
+++ cve_searchsploit/main.py
@@ -24,14 +24,15 @@ cve_map = {}
 
 ########## Library functions
 
+
 def iter_edbid_from_cve(cve):
     cve = cve.upper()
     if cve not in cve_map:
         return
 
-    with open(pdir + "/exploit-database/files_exploits.csv") as files:
+    with open(pdir + "/exploitdb/files_exploits.csv") as files:
         reader = csv.reader(files)
-        #reader.next() #skip header
+        # reader.next() #skip header
         next(reader)
 
         for row in reader:
@@ -41,8 +42,10 @@ def iter_edbid_from_cve(cve):
 
     return
 
+
 def edbid_from_cve(cve):
     return list(iter_edbid_from_cve(cve))
+
 
 def iter_cve_from_edbid(edb):
     edb = str(int(edb))
@@ -51,10 +54,13 @@ def iter_cve_from_edbid(edb):
         if edb in cve_map[cve]:
             yield cve.upper()
 
+
 def cve_from_edbid(edb):
     return list(iter_cve_from_edbid(edb))
 
+
 ##########
+
 
 def update_db():
     data = {}
@@ -66,42 +72,51 @@ def update_db():
         with open(pdir + "/exploitdb_mapping.json") as data_file:
             data = json.load(data_file)
 
-    print ("Refreshing exploit-database repo with latest exploits")
-    os.system("cd %s/exploit-database/; git pull origin master" % pdir)
+    print("Refreshing exploit-database repo with latest exploits")
+    os.system("cd %s/exploitdb/; git pull origin main" % pdir)
 
-    files = open(pdir + "/exploit-database/files_exploits.csv")
+    files = open(pdir + "/exploitdb/files_exploits.csv")
     reader = csv.reader(files)
-    #reader.next() #skip header
+    # reader.next() #skip header
     next(reader)
 
     reader = list(reader)
     csv_len = len(reader)
 
-    get_header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    get_header = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+    }
 
     def locations_of_substring(string, substring):
         substring_length = len(substring)
+
         def recurse(locations_found, start):
             location = string.find(substring, start)
             if location != -1:
-                return recurse(locations_found + [location], location+substring_length)
+                return recurse(
+                    locations_found + [location], location + substring_length
+                )
             else:
                 return locations_found
+
         return recurse([], 0)
 
-    print ("Refreshing EDBID-CVE mapping")
+    print("Refreshing EDBID-CVE mapping")
     with progressbar.ProgressBar(max_value=csv_len) as bar:
         for i in range(csv_len):
             edb = tuple(reader[i])[0]
             if edb in data:
-                #print "Skipping edb id " + edb
+                # print "Skipping edb id " + edb
                 pass
             else:
-                #print "Downloading https://www.exploit-db.com/exploits/" + edb
+                # print "Downloading https://www.exploit-db.com/exploits/" + edb
                 content = ""
                 while True:
                     try:
-                        r = requests.get("https://www.exploit-db.com/exploits/" + edb, headers=get_header)
+                        r = requests.get(
+                            "https://www.exploit-db.com/exploits/" + edb,
+                            headers=get_header,
+                        )
                         content = r.text
                     except Exception:
                         time.sleep(10)
@@ -109,28 +124,44 @@ def update_db():
                     finally:
                         break
                 used = []
-                indexes = locations_of_substring(content, 'https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-')
+                indexes = locations_of_substring(
+                    content, "https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-"
+                )
                 for pos in indexes:
-                      cve = r.text[pos + len('https://cve.mitre.org/cgi-bin/cvename.cgi?name='): pos + len('https://cve.mitre.org/cgi-bin/cvename.cgi?name=') + 9].upper()
-                      pos += len('https://cve.mitre.org/cgi-bin/cvename.cgi?name=') + 9
-                      while pos < len(r.text) and r.text[pos].isdigit():
-                          cve += r.text[pos]
-                          pos += 1
-                      cve = cve.replace("\u2013", "-")
-                      if cve in used: continue
-                      used.append(cve)
-                      print ("Found: edbid " + edb + " <---> " + cve)
-                indexes = locations_of_substring(content, 'https://nvd.nist.gov/vuln/detail/CVE-')
+                    cve = r.text[
+                        pos
+                        + len("https://cve.mitre.org/cgi-bin/cvename.cgi?name=") : pos
+                        + len("https://cve.mitre.org/cgi-bin/cvename.cgi?name=")
+                        + 9
+                    ].upper()
+                    pos += len("https://cve.mitre.org/cgi-bin/cvename.cgi?name=") + 9
+                    while pos < len(r.text) and r.text[pos].isdigit():
+                        cve += r.text[pos]
+                        pos += 1
+                    cve = cve.replace("\u2013", "-")
+                    if cve in used:
+                        continue
+                    used.append(cve)
+                    print("Found: edbid " + edb + " <---> " + cve)
+                indexes = locations_of_substring(
+                    content, "https://nvd.nist.gov/vuln/detail/CVE-"
+                )
                 for pos in indexes:
-                      cve = r.text[pos + len('https://nvd.nist.gov/vuln/detail/'): pos + len('https://nvd.nist.gov/vuln/detail/') + 9].upper()
-                      pos += len('https://nvd.nist.gov/vuln/detail/') + 9
-                      while pos < len(r.text) and r.text[pos].isdigit():
-                          cve += r.text[pos]
-                          pos += 1
-                      cve = cve.replace("\u2013", "-")
-                      if cve in used: continue
-                      used.append(cve)
-                      print ("Found: edbid " + edb + " <---> " + cve)
+                    cve = r.text[
+                        pos
+                        + len("https://nvd.nist.gov/vuln/detail/") : pos
+                        + len("https://nvd.nist.gov/vuln/detail/")
+                        + 9
+                    ].upper()
+                    pos += len("https://nvd.nist.gov/vuln/detail/") + 9
+                    while pos < len(r.text) and r.text[pos].isdigit():
+                        cve += r.text[pos]
+                        pos += 1
+                    cve = cve.replace("\u2013", "-")
+                    if cve in used:
+                        continue
+                    used.append(cve)
+                    print("Found: edbid " + edb + " <---> " + cve)
                 data[edb] = used
                 time.sleep(random.uniform(0.1, 0.3))
             bar.update(i)
@@ -149,9 +180,9 @@ def update_db():
 
 
 def _search_cve_aux(cve):
-    files = open(pdir + "/exploit-database/files_exploits.csv")
+    files = open(pdir + "/exploitdb/files_exploits.csv")
     reader = csv.reader(files)
-    #reader.next() #skip header
+    # reader.next() #skip header
     next(reader)
 
     found = False
@@ -159,21 +190,22 @@ def _search_cve_aux(cve):
         edb, file, description, date, author, platform, type, port = tuple(row)
         if edb in cve_map[cve]:
             found = True
-            print (" Exploit DB Id: " + edb)
-            print (" File: " + pdir + "/exploit-database/" + file)
-            print (" Date: " + date)
-            print (" Author: " + author)
-            print (" Platform: " + platform)
-            print (" Type: " + type)
+            print(" Exploit DB Id: " + edb)
+            print(" File: " + pdir + "/exploitdb/" + file)
+            print(" Date: " + date)
+            print(" Author: " + author)
+            print(" Platform: " + platform)
+            print(" Type: " + type)
             if port != "0":
-                print (" Port: " + port)
-            print ("")
+                print(" Port: " + port)
+            print("")
     if not found:
-        print ("ERROR - No EDB Id found")
-        print ("")
+        print("ERROR - No EDB Id found")
+        print("")
 
     files.close()
     return found
+
 
 def search_from_file(file):
     for line in file:
@@ -183,21 +215,22 @@ def search_from_file(file):
 
         cve = line.upper()
         sname = "| " + cve + " |"
-        print ("+" + "-" * (len(sname)-2) + "+")
-        print (sname)
-        print ("+" + "-" * (len(sname)-2) + "+")
-        print ("")
+        print("+" + "-" * (len(sname) - 2) + "+")
+        print(sname)
+        print("+" + "-" * (len(sname) - 2) + "+")
+        print("")
 
         if not cve in cve_map:
-            print ("ERROR - CVE not found.")
-            print ("")
+            print("ERROR - CVE not found.")
+            print("")
             continue
 
         _search_cve_aux(cve)
 
+
 def search_from_nessus(file):
     reader = csv.reader(file)
-    #reader.next() #skip header
+    # reader.next() #skip header
     next(reader)
 
     for row in reader:
@@ -210,32 +243,33 @@ def search_from_nessus(file):
             continue
 
         sname = "| " + name + " |"
-        print ("+" + "-" * (len(sname)-2) + "+")
-        print (sname)
-        print ("+" + "-" * (len(sname)-2) + "+")
-        print ("")
-        print (" CVE: " + cve)
-        print (" Protocol: " + proto)
-        print (" Port: " + port)
-        print ("")
-        print (" +----+ Exploit DB matching +----+ ")
-        print ("")
+        print("+" + "-" * (len(sname) - 2) + "+")
+        print(sname)
+        print("+" + "-" * (len(sname) - 2) + "+")
+        print("")
+        print(" CVE: " + cve)
+        print(" Protocol: " + proto)
+        print(" Port: " + port)
+        print("")
+        print(" +----+ Exploit DB matching +----+ ")
+        print("")
 
         _search_cve_aux(cve)
-        print ("")
+        print("")
+
 
 def search_cve(cve):
     cve = cve.upper()
 
     sname = "| " + cve + " |"
-    print ("+" + "-" * (len(sname)-2) + "+")
-    print (sname)
-    print ("+" + "-" * (len(sname)-2) + "+")
-    print ("")
+    print("+" + "-" * (len(sname) - 2) + "+")
+    print(sname)
+    print("+" + "-" * (len(sname) - 2) + "+")
+    print("")
 
     if not cve in cve_map:
-        print ("ERROR - CVE not found.")
-        print ("")
+        print("ERROR - CVE not found.")
+        print("")
         sys.exit(1)
 
     found = _search_cve_aux(cve)
@@ -244,21 +278,24 @@ def search_cve(cve):
 
 
 def usage():
-    print ("+------------------------------------+")
-    print ("|          cve_searchsploit          |")
-    print ("| Copyright 2017-19, Andrea Fioraldi |")
-    print ("+------------------------------------+")
-    print ("")
-    print ("Usage:")
-    print ("  python3 cve_searchsploit.py [parameters...]")
-    print ("")
-    print ("Parameters:")
-    print ("  <cve>                      search exploits by a cve")
-    print ("  -u                         update the cve-edbid database")
-    print ("  -f <file with cve list>    search exploits by a cve list file")
-    print ("  -n <nessus csv scan file>  search exploits by the cve matching with a nessus scan in csv format")
-    print ("")
+    print("+------------------------------------+")
+    print("|          cve_searchsploit          |")
+    print("| Copyright 2017-19, Andrea Fioraldi |")
+    print("+------------------------------------+")
+    print("")
+    print("Usage:")
+    print("  python3 cve_searchsploit.py [parameters...]")
+    print("")
+    print("Parameters:")
+    print("  <cve>                      search exploits by a cve")
+    print("  -u                         update the cve-edbid database")
+    print("  -f <file with cve list>    search exploits by a cve list file")
+    print(
+        "  -n <nessus csv scan file>  search exploits by the cve matching with a nessus scan in csv format"
+    )
+    print("")
     sys.exit(1)
+
 
 def main():
     global cve_map
@@ -272,36 +309,38 @@ def main():
     for i in range(1, len(sys.argv)):
         a = sys.argv[i]
         if a == "-u":
-            print ("ERROR - '-u' is mutually exclusive with all the other arguments")
-            print ("")
+            print("ERROR - '-u' is mutually exclusive with all the other arguments")
+            print("")
             sys.exit(1)
         elif a == "-f":
-            if i +1 == len(sys.argv):
+            if i + 1 == len(sys.argv):
                 usage()
             try:
-                file = open(sys.argv[i+1], "r")
+                file = open(sys.argv[i + 1], "r")
                 search_from_file(file)
             except Exception as exc:
-                print ("ERROR - " + str(exc))
-                print ("")
+                print("ERROR - " + str(exc))
+                print("")
                 sys.exit(1)
         elif a == "-n":
-            if i +1 == len(sys.argv):
+            if i + 1 == len(sys.argv):
                 usage()
             try:
-                file = open(sys.argv[i+1], "r")
+                file = open(sys.argv[i + 1], "r")
                 search_from_nessus(file)
             except Exception as exc:
-                print ("ERROR - " + str(exc))
-                print ("")
+                print("ERROR - " + str(exc))
+                print("")
                 sys.exit(1)
         else:
             search_cve(a)
 
 
-if not os.path.isdir(pdir + "/exploit-database"):
-    print ("Cloning exploit-database repository")
-    os.system("cd %s; git clone https://github.com/offensive-security/exploit-database" % pdir)
+if not os.path.isdir(pdir + "/exploitdb"):
+    print("Cloning exploit-database repository")
+    os.system(
+        "cd %s; git clone https://gitlab.com/exploit-database/exploitdb.git" % pdir
+    )
 
 with open(pdir + "/exploitdb_mapping_cve.json") as data_file:
     cve_map = json.load(data_file)
